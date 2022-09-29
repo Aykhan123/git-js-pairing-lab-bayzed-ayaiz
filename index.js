a7b54557df2ef174b3427f27544e72bcb2475876
aykhan123 <ayaizkhan59@gmail.com>
2022-09-29 18:51:08 +0000

    ayaiz did the third one

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ function fiveToOneHundred(){
     for (let i = 5; i <= 100; i += 1)
     console.log(i);
 }
-console.log(fiveToOneHundred());
+//console.log(fiveToOneHundred());
 
 //2
 const multiplesOfThree = () => {
   for (let i = 3; i <= 100; i += 3) {
-      console.log(i);
+      //console.log(i);
   }  
 };
 multiplesOfThree();
+
+//3
+function multiplesOfThreeOrFive(){
+    for (let i = 3; i <=100; i += 1){
+        if ((i % 3 ===0) || (i % 5 ===0 )){
+            console.log(i)
+        }
+    }
+    
+}
+multiplesOfThreeOrFive()
