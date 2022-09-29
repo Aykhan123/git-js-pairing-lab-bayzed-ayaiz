fb8cb964dd71dcaa235b240499cb0da5ae8f17be
BayzU <bayzedullah@gmail.com>
2022-09-29 18:37:51 +0000

    Bayzed did the second one

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function fiveToOneHundred(){
     console.log(i);
 }
 console.log(fiveToOneHundred());
+
+//2
+const multiplesOfThree = () => {
+  for (let i = 3; i <= 100; i += 3) {
+      console.log(i);
+  }  
+};
+multiplesOfThree();
